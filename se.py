7c5f3bd9209e3ccda4ae91b1a0d488bf99833fe4
Juhi Patel <--list>
2020-10-17 03:02:45 +0000

    Google search result file added

--- se.py
+++ se.py
@@ -1,4 +1,5 @@
-import pprint
+#import pprint
+import json
 from googleapiclient.discovery import build
 
 my_api_key = config.api_key
@@ -11,6 +12,13 @@ def google_search(search_term, api_key, cse_id, **kwargs):
     return res['items']
 
 if __name__ == '__main__':
-    results = google_search(my_search_topic, my_api_key, my_cse_id, num=10)
-    pprint.pprint(results)
+    total_page_number =10
+    results =[]
+
+    for page_number in range(total_page_number):
+        current_result = google_search(my_search_topic, my_api_key, my_cse_id, num=10,start=page_number*10+1)
+        results += current_result
+        print(len(results))
+    with open('google_search.json', 'w') as dump_file:
+        json.dump(results, dump_file)
     
