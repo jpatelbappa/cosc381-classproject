a6e611200a17c83c7dd6ca0e52244943436b4fdd
Juhi Patel <--list>
2020-10-21 14:14:38 +0000

    add file

---
+++ fizzbuzz.py
@@ -0,0 +1,17 @@
+def fizz_buzz(limit):
+    for i in range(limit + 1):
+        if i % 3 == 0:
+            print('fizz')
+        if i % 5 == 0:
+            print('buzz')
+        if i % 3 and i % 5:
+            print(i)
+
+def main():
+    print ("MY NAME IS JUHI PATEL")
+    fizz_buzz(10)
+
+if _name_ == "__main__":
+    main()
+   
+
